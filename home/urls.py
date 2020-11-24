ff265fa34f6c97d38f6b3c320e2e915d9e797c8a
prajakta mane <prajakta916mane1@gmail.com>
2020-11-24 07:54:31 +0530

    about us

--- home/urls.py
+++ home/urls.py
@@ -7,4 +7,9 @@ app_name = 'home'
 
 urlpatterns = [
     path('',views.home , name='home' ),
+    path('manager',views.manager,name='manager'),
+    path('manager/about_us',views.about_us,name="about_us"),
+    path('manager/chef',views.about_us,name="about_us"),
+    path('manager/why_choose_us',views.about_us,name="about_us"),
+    path("manager/updateAboutus",views.updateAboutus, name="updateAboutus"),
 ]
--- home/views.py
+++ home/views.py
@@ -1,7 +1,7 @@
-from django.shortcuts import render
+from django.shortcuts import render,redirect
 from meals.models import Meals , Category
 from blog.models import Post
-from aboutus.models import Why_Choose_Us
+from aboutus.models import Why_Choose_Us ,AboutUs
 # Create your views here.
 
 
@@ -26,3 +26,22 @@ def home(request):
     }
 
     return render(request , 'home/index.html' , context)
+
+def manager(request):
+    return render(request , 'home/manager.html')
+
+def about_us(request):
+    obj = AboutUs.objects.all()
+    context = {'about_us':obj}
+    return render(request , 'home/about_us.html',context)
+
+def updateAboutus(request):
+    if request.method == 'POST':
+        title = request.POST['title']
+        content = request.POST['content']
+        old_title = request.POST['old_title']
+        obj = AboutUs.objects.get(title=old_title)
+        obj.title = title
+        obj.content = content
+        obj.save()
+    return redirect('/manager/')
